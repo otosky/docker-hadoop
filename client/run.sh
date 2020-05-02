53926c9e19cfa15fa3942be9bb56a6c7a772b5d4
Oliver Tosky <olivertosky@Olivers-MacBook-Pro.local>
2020-05-02 17:55:51 -0400

    Add spark conf defaults for yarn

--- client/run.sh
+++ client/run.sh
@@ -8,4 +8,7 @@ hdfs dfs -copyFromLocal /tmp/tez-$TEZ_VERSION /apps/
 rm -r /tmp/tez-$TEZ_VERSION
 rm -r /tmp/tez
 
+hdfs dfs -mkdir -p /apps/spark
+hdfs dfs -copyFromLocal $SPARK_HOME/jars /apps/spark
+
 tail -F /dev/null
